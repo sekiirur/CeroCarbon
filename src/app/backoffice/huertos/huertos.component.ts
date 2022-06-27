1ef56115dc116a5b5664dd26205d13e4f109cffb
Sergio Tejera <sergiotejera1999@gmail.com>
2022-06-27 14:25:42 +0200

    modified:   src/app/backoffice/huertos/huertos.component.ts 	modified:   src/app/entities/huertos/huerto.service.ts 	modified:   src/app/home/home.component.css 	modified:   src/app/home/home.component.html

--- src/app/backoffice/huertos/huertos.component.ts
+++ src/app/backoffice/huertos/huertos.component.ts
@@ -10,11 +10,30 @@ import { Huerto } from 'src/app/models/Huerto';
 export class HuertosComponent implements OnInit {
 
   listaHuertos: Huerto[] = [];
+
+  display = "none";
+
+  huertoEliminar = 0;
+  error?: boolean;
+  mensaje?:String;
+
+  page:number = 0;
+  size:number = 5;
+  sort:String = "id,asc";
+
+  orden:boolean = false; //false ascendente, true descendente
+
+
+  first:boolean = false;
+  last:boolean = false;
+  totalPaginas:number  = 0;
+  totalHuertos:number = 0;
+
   
   constructor(protected huertoService: HuertoService) { }
 
   ngOnInit() {
-    this.cargarDatos();
+    this.cargarDatosPaginados(this.page,this.size,this.sort);
   }
 
   cargarDatos(){
@@ -22,6 +41,67 @@ export class HuertosComponent implements OnInit {
     this.huertoService.obtenerHuertos().subscribe(result=>{
       this.listaHuertos=result;
     });
+  }
+
+  cambiarOrden(cadena:String){
+    console.log(this.orden);
+    this.orden = !this.orden;
+    this.sort = `${cadena},${this.orden ? 'asc' : 'desc'}`
+    this.cargarDatosPaginados(this.page, this.size, this.sort);
+
+  }
+
+  cargarDatosPaginados(page:number, size:number, sort:String){
+    this.huertoService.obtenerHuertos().subscribe(result=>{
+      this.listaHuertos=result;
+    });
+    this.huertoService.obtenerHuertosPaginados(page,size,sort).subscribe(res=>{
+      this.listaHuertos = res.content;
+      this.first = res.first;
+      this.last = res.last;
+      this.totalPaginas = res.totalPages;
+      this.totalHuertos = res.totalElements; 
+    });
+  }
+
+  navegarNext(){
+    this.page+=1;
+    this.cargarDatosPaginados(this.page, this.size, this.sort);
+  }
+
+  navegarPrev(){
+    this.page-=1;
+    this.cargarDatosPaginados(this.page, this.size, this.sort);
+  }
+
+  abrirPopUpEliminar(id:number){
+    this.display = 'block'; 
+    this.huertoEliminar = id;
+  }
+
+  confirmarEliminar(huerto: number){
+    this.huertoService.eliminarHuerto(huerto).subscribe(
+      res => {
+      this.huertoService.obtenerHuertos().subscribe(
+        resp =>{
+          this.listaHuertos = resp;
+        }
+      );
+      
+      this.error = false;
+      this.mensaje = '';
+      this.cerrarModalEliminar();
+      }, err=>{
+        this.cerrarModalEliminar();
+        this.error = true;
+        this.mensaje= 'Se ha producido un error al borrar el huerto '+ err.error;
+      }
+    );
+    this.cerrarModalEliminar();
+  }
+
+  cerrarModalEliminar(){
+    this.display = 'none';
   }
 }
 
--- src/app/entities/huertos/huerto.service.ts
+++ src/app/entities/huertos/huerto.service.ts
@@ -14,8 +14,33 @@ export class HuertoService {
     return this.http.get<Huerto[]>(url);
   }
 
+  public obtenerHuertosPaginados(page:number, size:number, sort:String ):Observable<any>{
+    //page=0&size=3&sort=id,asc
+    const params = `?page=${page}&size=${size}&sort=${sort}`;
+    const url = "http://localhost:8080/api/huertos-paginados"+params;
+    return this.http.get<any>(url);
+
+  }
+
   public guardarHuerto(Huerto: Huerto): Observable<Huerto>{
     const URL = 'http://localhost:8080/api/huertos';
     return this.http.post<Huerto>(URL, Huerto);
   }
+
+  public obtenerHuertoPorId(huertoID:number): Observable<Huerto>{
+    const URL = 'http://localhost:8080/api/huertos/'+ huertoID;
+    return this.http.get<Huerto>(URL);
+  }
+
+  public editarHuerto(huertoID:number):Observable<Huerto>{
+    const URL = 'http://localhost:8080/api/huertos/'+ huertoID;
+    return this.http.put<Huerto>(URL, Huerto);
+  }
+
+  public eliminarHuerto(huertoID:number): Observable<void>{
+    const URL = 'http://localhost:8080/api/huertos/'+ huertoID;
+    return this.http.delete<void>(URL);
+  }
+
 }
+
